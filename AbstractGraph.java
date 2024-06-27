a027abf27c3bbccfa8af1cd3c5b926c47214950d
nda122 <96876598+nda122@users.noreply.github.com>
2024-06-28 00:21:57 +0700

    Update AbstractGraph.java

--- AbstractGraph.java
+++ AbstractGraph.java
@@ -1,7 +1,17 @@
 package Termproject;
 import java.util.*;
 interface Graph{
-    public void BFS(int start);
-    public void DFS(int start);
+    public ArrayList<Integer> BFS(int start);
+    public ArrayList<Integer> DFS(int start);
+    
+}
+
+interface DirectedGraph extends Graph{
+    public boolean checkCycle();
+    public ArrayList<Integer> TopologicalSorting();
+    
+}
+interface UndirectedGraph extends Graph{
+    public boolean checkCycle();
     
 }
